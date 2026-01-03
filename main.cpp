2e2eb1a0652cb86dec5cd8da4a3d78d356892704
cxinu <72374438+cxinu@users.noreply.github.com>
2026-01-03 13:05:00 +0530

    add Cuda assert, revert window size from fixed to image.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include "kernel.h"
+#include <cuda_runtime.h>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+
+#define checkCuda(ans)                                                         \
+	{                                                                          \
+		gpuAssert((ans), __FILE__, __LINE__);                                  \
+	}
+
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+					  bool abort = true) {
+	if (code != cudaSuccess) {
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+				line);
+		if (abort)
+			exit(code);
+	}
+}
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -15,23 +31,25 @@ int main(int argc, char **argv) {
 	}
 	cv::Mat output_full = cv::Mat::zeros(input_full.size(), input_full.type());
 
+	auto start = std::chrono::high_resolution_clock::now();
+
 	// INFO: CUDA kernel
 	launch_sobel(input_full.data, output_full.data, input_full.cols,
 				 input_full.rows);
 
-	cv::Mat display_in, display_out;
-	cv::Size displaySize(600, 600);
-
-	cv::resize(input_full, display_in, displaySize);
-	cv::resize(output_full, display_out, displaySize);
+	cudaDeviceSynchronize();
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> ms = end - start;
+	std::cout << "Kernel Execution Time: " << ms.count() << " ms" << std::endl;
 
 	cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
 	cv::namedWindow("Sobel Edge Detection", cv::WINDOW_AUTOSIZE);
+
 	cv::moveWindow("Original", 100, 100);
 	cv::moveWindow("Sobel Edge Detection", 700, 100);
 
-	cv::imshow("Original", display_in);
-	cv::imshow("Sobel Edge Detection", display_out);
+	cv::imshow("Original", input_full);
+	cv::imshow("Sobel Edge Detection", output_full);
 
 	std::cout << "Windows active. Press 'q' or ESC to exit safely."
 			  << std::endl;
